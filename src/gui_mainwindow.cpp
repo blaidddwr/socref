07d99db32639a36e6cdd35b88e1da377e8d0fe8a
blaidddwr <blaidddwr@protonmail.com>
2017-10-13 11:17:04 -0700

    Worked on mainwindow class.
    Hit a roadblock where a concrete project factory class must be defined
    and set until mainwindow can be worked on further.

--- src/gui_mainwindow.cpp
+++ src/gui_mainwindow.cpp
@@ -1,11 +1,180 @@
-#include "gui_mainwindow.h"
+#include <QAction>
+#include <QMenuBar>
 
-MainWindow::MainWindow(QWidget *parent)
-   : QMainWindow(parent)
+#include "gui_mainwindow.h"
+#include "abstractprojectfactory.h"
+#include "project.h"
+
+
+
+using namespace Gui;
+
+
+
+
+
+
+//@@
+MainWindow::MainWindow(QWidget *parent):
+   QMainWindow(parent)
+{
+   createActions();
+   createMenus();
+   setWindowTitle(tr("Socrates' Reference"));
+}
+
+
+
+
+
+
+//@@
+void MainWindow::setProject(Project* project)
+{
+   if ( _project )
+   {
+      delete _project;
+   }
+   _project = project;
+   project->setParent(this);
+}
+
+
+
+
+
+
+//@@
+void MainWindow::newTriggered()
 {
 }
 
-MainWindow::~MainWindow()
-{
 
+
+
+
+
+//@@
+void MainWindow::openTriggered()
+{
+}
+
+
+
+
+
+
+//@@
+void MainWindow::saveTriggered()
+{
+}
+
+
+
+
+
+
+//@@
+void MainWindow::saveAsTriggered()
+{
+}
+
+
+
+
+
+
+//@@
+void MainWindow::closeTriggered()
+{
+}
+
+
+
+
+
+
+//@@
+void MainWindow::projectSettingsTriggered()
+{
+}
+
+
+
+
+
+
+//@@
+void MainWindow::createActions()
+{
+   // create new actions for each project type
+   /*AbstractProjectFactory& factory = AbstractProjectFactory::getInstance();
+   for (int i = 0; i < factory.getSize() ;++i)
+   {
+      _newActions.append(new QAction(factory.getName(i),this));
+      _newActions.back()->setData(i);
+      connect(_newActions.back(),&QAction::triggered,this,&MainWindow::newTriggered);
+   }*/
+
+   // create open action
+   _openAction = new QAction(tr("&Open"),this);
+   _openAction->setShortcut(QKeySequence::Open);
+   _openAction->setStatusTip(tr("Open an existing project."));
+   connect(_openAction,&QAction::triggered,this,&MainWindow::openTriggered);
+
+   // create save action
+   _saveAction = new QAction(tr("&Save"),this);
+   _saveAction->setShortcut(QKeySequence::Save);
+   _saveAction->setStatusTip(tr("Save current project."));
+   _saveAction->setDisabled(true);
+   connect(_saveAction,&QAction::triggered,this,&MainWindow::saveTriggered);
+
+   // create save as action
+   _saveAsAction = new QAction(tr("Save &As"),this);
+   _saveAsAction->setStatusTip(tr("Save current project under a new path."));
+   _saveAsAction->setDisabled(true);
+   connect(_saveAsAction,&QAction::triggered,this,&MainWindow::saveAsTriggered);
+
+   // create close action
+   _closeAction = new QAction(tr("&Close"),this);
+   _closeAction->setShortcut(QKeySequence::Close);
+   _closeAction->setStatusTip(tr("Close the current project."));
+   _closeAction->setDisabled(true);
+   connect(_closeAction,&QAction::triggered,this,&MainWindow::closeTriggered);
+
+   // create project settings action
+   _projectSettingsAction = new QAction(tr("&Project"),this);
+   _projectSettingsAction->setStatusTip(tr("Edit basic settings of project"));
+   _projectSettingsAction->setDisabled(true);
+   connect(_projectSettingsAction,&QAction::triggered,this,&MainWindow::projectSettingsTriggered);
+}
+
+
+
+
+
+
+//@@
+void MainWindow::createMenus()
+{
+   // make file menu
+   QMenu* fileMenu = menuBar()->addMenu(tr("&File"));
+
+   // make new submenu for each project type
+   /*QMenu* newMenu = fileMenu->addMenu(tr("&New"));
+   AbstractProjectFactory& factory = AbstractProjectFactory::getInstance();
+   for (int i = 0; i < factory.getSize() ;++i)
+   {
+      newMenu->addAction(_newActions.at(i));
+   }*/
+
+   // finish making file menu
+   fileMenu->addAction(_openAction);
+   fileMenu->addAction(_saveAction);
+   fileMenu->addAction(_saveAsAction);
+   fileMenu->addAction(_closeAction);
+
+   // make settings menu
+   QMenu* settingsMenu = menuBar()->addMenu(tr("&Settings"));
+   settingsMenu->addAction(_projectSettingsAction);
 }
--- src/gui_mainwindow.h
+++ src/gui_mainwindow.h
@@ -1,15 +1,44 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
 
+
+
+class Project;
+
+
+
+//@@
+namespace Gui {
+
+//@@
 class MainWindow : public QMainWindow
 {
    Q_OBJECT
-
 public:
-   MainWindow(QWidget *parent = 0);
-   ~MainWindow();
+   MainWindow(QWidget* parent = nullptr);
+   void setProject(Project* project);
+private slots:
+   void newTriggered();
+   void openTriggered();
+   void saveTriggered();
+   void saveAsTriggered();
+   void closeTriggered();
+   void projectSettingsTriggered();
+private:
+   void createActions();
+   void createMenus();
+   Project* _project {nullptr};
+   QVector<QAction*> _newActions;
+   QAction* _openAction;
+   QAction* _saveAction;
+   QAction* _saveAsAction;
+   QAction* _closeAction;
+   QAction* _projectSettingsAction;
 };
 
-#endif // MAINWINDOW_H
+} // namespace Gui
+
+
+
+#endif
