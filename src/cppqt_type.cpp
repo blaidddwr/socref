1e804b31945bf5bb989ce00265fe82dc33e1658d
blaidddwr <blaidddwr@protonmail.com>
2017-11-03 23:07:19 -0700

    Worked on C++/Qt type class.
    Changed class so it is much more editable along with having template
    arguments that can be set to values or left unset.

--- src/cppqt_type.cpp
+++ src/cppqt_type.cpp
@@ -13,46 +13,6 @@ using namespace CppQt;
 
 
 //@@
-Type Type::toType(const QString& pointer, bool constantValue, bool constantPointer) const
-{
-   // make sure pointer string is correct
-   QRegExp regexp("\\**\\&?");
-   if ( !regexp.exactMatch(pointer) )
-   {
-      Exception::InvalidArgument e;
-      MARK_EXCEPTION(e);
-      e.setDetails(QObject::tr("Invalid pointer string '%1' given for deriving new type.")
-                   .arg(pointer));
-      throw e;
-   }
-
-   // if constant pointer is requested make sure pointer type is of correct type
-   regexp = QRegExp("\\*+");
-   if ( constantPointer && !regexp.exactMatch(pointer) )
-   {
-      Exception::InvalidUse e;
-      MARK_EXCEPTION(e);
-      e.setDetails(QObject::tr("Cannot set type as constant pointer with pointer string '%1'.")
-                   .arg(pointer));
-      throw e;
-   }
-
-   // create new type from this type's data
-   Type ret(_scope,_name,_templateArguments);
-
-   // set new data to new type and return it
-   ret._pointer = pointer;
-   ret._constantValue = constantValue;
-   ret._constantPointer = constantPointer;
-   return ret;
-}
-
-
-
-
-
-
-//@@
 QString Type::getName(const QList<QString>& scope) const
 {
    // create return string
@@ -93,16 +53,28 @@ QString Type::getName(const QList<QString>& scope) const
    // append basic type name
    ret.append(_name).append(_pointer);
 
-   // check if type has any template arguments
-   if ( !_templateArguments.isEmpty() )
+   // check if template values are set
+   if ( !_templateValues.isEmpty() )
+   {
+      // iterate through all template values and append it to type name string
+      ret.append("<");
+      for (int i = 0; i < (_templateValues.size() - 1) ;++i)
+      {
+         ret.append(_templateValues.at(i).getName(scope)).append(",");
+      }
+      ret.append(_templateValues.constLast().getName(scope)).append(">");
+   }
+
+   // else check if type has any template arguments
+   else if ( !_templateArguments.isEmpty() )
    {
       // iterate through all template arguments and append it to type name string
       ret.append("<");
       for (int i = 0; i < (_templateArguments.size() - 1) ;++i)
       {
-         ret.append(_templateArguments.at(i).getName(scope)).append(",");
+         ret.append(_templateArguments.at(i)).append(",");
       }
-      ret.append(_templateArguments.constLast().getName(scope)).append(">");
+      ret.append(_templateArguments.constLast()).append(">");
    }
 
    // if type is constant pointer append it to string
@@ -113,4 +85,123 @@ QString Type::getName(const QList<QString>& scope) const
 
    // return constructed type name
    return ret;
+}
+
+
+
+
+
+
+//@@
+Type& Type::setTemplateValues(const QList<Type>& values)
+{
+   // make sure list size of values is the same as size of arguments
+   if ( values.size() != _templateArguments.size() )
+   {
+      Exception::InvalidArgument e;
+      MARK_EXCEPTION(e);
+      e.setDetails(QObject::tr("Cannot set template values from list of size %1 when arguments list"
+                               " is size %2.").arg(values.size()).arg(_templateArguments.size()));
+      throw e;
+   }
+
+   // set template values and return referene to this
+   _templateValues = values;
+   return *this;
+}
+
+
+
+
+
+
+//@@
+Type& Type::clearTemplateValues()
+{
+   // clear template values and return reference to this
+   _templateValues.clear();
+   return *this;
+}
+
+
+
+
+
+
+//@@
+Type& Type::setPointer(const QString& pointer)
+{
+   // make sure pointer string is correct
+   if ( !QRegExp("\\**\\&?").exactMatch(pointer) )
+   {
+      Exception::InvalidArgument e;
+      MARK_EXCEPTION(e);
+      e.setDetails(QObject::tr("Invalid pointer string '%1' given for deriving new type.")
+                   .arg(pointer));
+      throw e;
+   }
+
+   // if constant pointer is set make sure pointer type is of correct type
+   if ( _constantPointer && !QRegExp("\\*+").exactMatch(pointer) )
+   {
+      Exception::InvalidUse e;
+      MARK_EXCEPTION(e);
+      e.setDetails(QObject::tr("Cannot set type as constant pointer with pointer string '%1'.")
+                   .arg(pointer));
+      throw e;
+   }
+
+   // set new pointer string and return reference to this
+   _pointer = pointer;
+   return *this;
+}
+
+
+
+
+
+
+//@@
+Type& Type::setConstants(bool constantValue, bool constantPointer)
+{
+   // if constant pointer is set make sure pointer type is of correct type
+   if ( constantPointer && !QRegExp("\\*+").exactMatch(_pointer) )
+   {
+      Exception::InvalidUse e;
+      MARK_EXCEPTION(e);
+      e.setDetails(QObject::tr("Cannot set type as constant pointer with pointer string '%1'.")
+                   .arg(_pointer));
+      throw e;
+   }
+
+   // set constant values and return reference to this
+   _constantValue = constantValue;
+   _constantPointer = constantPointer;
+   return *this;
+}
+
+
+
+
+
+
+//@@
+Type& Type::appendNamespace(const QString& name)
+{
+   // append new namespace and return reference to this
+   _scope.append(name);
+   return *this;
+}
+
+
+
+
+
+
+//@@
+Type& Type::prependNamespace(const QString& name)
+{
+   // prepend new namespace and return reference to this
+   _scope.prepend(name);
+   return *this;
 }
--- src/cppqt_type.h
+++ src/cppqt_type.h
@@ -12,18 +12,30 @@ namespace CppQt
    {
    public:
       Type(const QString& name);
-      Type(const QString& name, const QList<Type>& templateArguments);
+      Type(const QString& name, const QList<QString>& templateArguments);
       Type(const QList<QString>& scope, const QString& name);
-      Type(const QList<QString>& scope, const QString& name, const QList<Type>& templateArguments);
-      Type toType(const QString& pointer, bool constantValue, bool constantPointer) const;
+      Type(const QList<QString>& scope, const QString& name
+           , const QList<QString>& templateArguments);
       QString getName(const QList<QString>& scope) const;
       bool isTemplate() const;
+      bool isConcrete() const;
+      int getTemplateSize() const;
+      QString getTemplateName(int index) const;
+      Type& setTemplateValues(const QList<Type>& values);
+      Type& clearTemplateValues();
+      Type& setPointer(const QString& pointer);
+      Type& setConstants(bool constantValue, bool constantPointer);
+      Type& appendNamespace(const QString& name);
+      Type& prependNamespace(const QString& name);
+      bool isConstantValue() const;
+      bool isConstantPointer() const;
       bool operator!=(const Type& type);
       bool operator==(const Type& type);
    private:
       QList<QString> _scope;
       QString _name;
-      QList<Type> _templateArguments;
+      QList<QString> _templateArguments;
+      QList<Type> _templateValues;
       QString _pointer;
       bool _constantValue {false};
       bool _constantPointer {false};
@@ -35,7 +47,7 @@ namespace CppQt
    inline Type::Type(const QString& name): _name(name) {}
 
    //@@
-   inline Type::Type(const QString& name, const QList<Type>& templateArguments): _name(name),
+   inline Type::Type(const QString& name, const QList<QString>& templateArguments): _name(name),
       _templateArguments(templateArguments) {}
 
    //@@
@@ -44,23 +56,39 @@ namespace CppQt
 
    //@@
    inline Type::Type(const QList<QString>& scope, const QString& name
-                     , const QList<Type>& templateArguments): _scope(scope), _name(name),
+                     , const QList<QString>& templateArguments): _scope(scope), _name(name),
       _templateArguments(templateArguments) {}
 
    //@@
    inline bool Type::isTemplate() const { return _templateArguments.isEmpty(); }
 
    //@@
+   inline bool Type::isConcrete() const
+   { return _templateArguments.size() == _templateValues.size(); }
+
+   //@@
+   inline int Type::getTemplateSize() const { return _templateArguments.size(); }
+
+   //@@
+   inline QString Type::getTemplateName(int index) const { return _templateArguments.at(index); }
+
+   //@@
+   inline bool Type::isConstantValue() const { return _constantValue; }
+
+   //@@
+   inline bool Type::isConstantPointer() const { return _constantPointer; }
+
+   //@@
    inline bool Type::operator!=(const Type& type) { return _scope != type._scope
             || _name != type._name || _templateArguments != type._templateArguments
-            || _pointer != type._pointer || _constantValue != type._constantValue
-            || _constantPointer != type._constantPointer; }
+            || _templateValues != type._templateValues || _pointer != type._pointer
+            || _constantValue != type._constantValue || _constantPointer != type._constantPointer; }
 
    //@@
    inline bool Type::operator==(const Type& type) { return _scope == type._scope
             && _name == type._name && _templateArguments == type._templateArguments
-            && _pointer == type._pointer && _constantValue == type._constantValue
-            && _constantPointer == type._constantPointer; }
+            && _templateValues == type._templateValues && _pointer == type._pointer
+            && _constantValue == type._constantValue && _constantPointer == type._constantPointer; }
 }
 
 
