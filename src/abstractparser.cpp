0b4c3644568f5409d45d5640691b24c975571cc6
blaidddwr <blaidddwr@protonmail.com>
2018-03-17 17:11:52 -0700

    Added comments to abstract parser.
    As a result changed the order of some functions in the class.

--- src/abstractparser.cpp
+++ src/abstractparser.cpp
@@ -9,6 +9,14 @@
 
 
 /*!
+ * This constructs a new parser object that will be a root or primary parser. 
+ * This means this parser will be assumed to be the root parser of a source 
+ * file. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. Allocate new resources required for this new parser object. 
  */
 AbstractParser::AbstractParser():
    _input(new QStringList),
@@ -23,8 +31,18 @@ AbstractParser::AbstractParser():
 
 
 /*!
+ * This constructs a new parser object that is a child of another parser object. 
+ * This sets up this object to use the resources of its root parser object. 
  *
- * @param parent  
+ * @param parent The parent parser object for this new parser object. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. Set this object's parent as the given parent parser, add this parser to 
+ *    its parent's list of children, and set this object's root parser pointer. 
+ *
+ * 2. Set all resource pointers to point to this object's root parser resources. 
  */
 AbstractParser::AbstractParser(AbstractParser* parent)
 {
@@ -44,6 +62,14 @@ AbstractParser::AbstractParser(AbstractParser* parent)
 
 
 /*!
+ * This deletes the dynamically allocated resources if this parser if it is the 
+ * root. If this is a child parser then this does nothing. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. If this is a root parser then delete all allocated resources else do 
+ *    nothing. 
  */
 AbstractParser::~AbstractParser()
 {
@@ -61,8 +87,26 @@ AbstractParser::~AbstractParser()
 
 
 /*!
+ * This parses the given file using this parser as the root parser. If this is a 
+ * child parser then this does nothing. 
  *
- * @param file  
+ * @param file Qt file handle to the file that is parsed. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. If this is not a root parser then do nothing and exit, else go to the next 
+ *    step. 
+ *
+ * 2. Set the read line index to the beginning, call the initialize interface, 
+ *    and read in all lines of the source file. 
+ *
+ * 3. Process the input lines. 
+ *
+ * 4. Process the output lines. 
+ *
+ * 5. Write the output to the source file if the output is different from the 
+ *    original. 
  */
 void AbstractParser::execute(QFile* file)
 {
@@ -81,6 +125,8 @@ void AbstractParser::execute(QFile* file)
 
 
 /*!
+ * This interface initializes this parser. The default implementation does 
+ * nothing. 
  */
 void AbstractParser::initialize()
 {}
@@ -91,8 +137,21 @@ void AbstractParser::initialize()
 
 
 /*!
+ * Signals this parser that the given child parser should be used for calling 
+ * the interface for reading lines until it returns false or the last line is 
+ * processed. If the given parser is not a direct child of this parser then an 
+ * exception is thrown. 
  *
- * @param child  
+ * @param child Pointer to the child parser whose interface for reading lines 
+ *              will be called from now on. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. If the given parser object's parent is not this parser then an exception 
+ *    is thrown, else go to the next step. 
+ *
+ * 2. Set the internal child pointer signal variable to the given parser. 
  */
 void AbstractParser::stepIntoChild(AbstractParser* child)
 {
@@ -112,6 +171,10 @@ void AbstractParser::stepIntoChild(AbstractParser* child)
 
 
 /*!
+ * Returns the indent, in spaces, that is prepended to each line added to the 
+ * output. 
+ *
+ * @return Number of spaces prepended to each added line of output. 
  */
 int AbstractParser::indent()
 {
@@ -124,8 +187,17 @@ int AbstractParser::indent()
 
 
 /*!
+ * Sets the indent, in spaces, that is prepended to each line added to output. 
  *
- * @param indent  
+ * @param indent The new indent value in spaces. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. If the given indent is less than 0 then throw an exception, else go to the 
+ *    next step. 
+ *
+ * 2. Set the root parser object's indent to the new value given. 
  */
 void AbstractParser::setIndent(int indent)
 {
@@ -145,8 +217,19 @@ void AbstractParser::setIndent(int indent)
 
 
 /*!
+ * Adds the given line as a new line to output, appending it to the end of any 
+ * existing lines. The line is prepended with a number of spaces equal to the 
+ * indent. 
  *
- * @param line  
+ * @param line The line that is prepended with the indent and appended to the 
+ *             output. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. Prepend the given line with spaces equal to the indent of the root parser. 
+ *
+ * 2. Append the given line to the list of output lines of the root parser. 
  */
 void AbstractParser::add(const QString& line)
 {
@@ -161,8 +244,10 @@ void AbstractParser::add(const QString& line)
 
 
 /*!
+ * Appends multiple lines to the output, each one being prepended with spaces 
+ * equal to the indent. 
  *
- * @param lines  
+ * @param lines Lines that are appended to the output. 
  */
 void AbstractParser::add(const QStringList& lines)
 {
@@ -175,8 +260,10 @@ void AbstractParser::add(const QStringList& lines)
 
 
 /*!
+ * Appends the given number of blank lines to the output. If the given number is 
+ * less than 1 then nothing is added to the output. 
  *
- * @param count  
+ * @param count The number of blank lines added to the output. 
  */
 void AbstractParser::add(int count)
 {
@@ -189,8 +276,21 @@ void AbstractParser::add(int count)
 
 
 /*!
+ * Reads in the source file, splitting them into lines, and prepares the output. 
  *
- * @param file  
+ * @param file The file used as input. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. Read all contents of the given file, storing it as this object's original 
+ *    contents. 
+ *
+ * 2. If reading in the file failed and caused an error then throw an exception, 
+ *    else go to the next step. 
+ *
+ * 3. Split the original content by line and save it to this object's list of 
+ *    input lines to be processed. 
  */
 void AbstractParser::read(QFile* file)
 {
@@ -203,7 +303,6 @@ void AbstractParser::read(QFile* file)
       throw e;
    }
    *_input = _original.split('\n');
-   _output->clear();
 }
 
 
@@ -212,6 +311,21 @@ void AbstractParser::read(QFile* file)
 
 
 /*!
+ * Processes the input of the source file, using the read line interface for 
+ * processing. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. While the root parser object's index is not at the end of the input line 
+ *    list do the proceeding steps. 
+ *
+ * 2. Call the read line interface with the next input line to process. If the 
+ *    interface returns false then break the while loop and exit this function. 
+ *
+ * 3. If the read line interface calls the step into child function then call 
+ *    the child's process input function and then set the child pointer signal 
+ *    back to null. 
  */
 void AbstractParser::processInput()
 {
@@ -232,6 +346,14 @@ void AbstractParser::processInput()
 
 
 /*!
+ * Processes the output by calling the make output interface. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. Clear the root parser object's output string list and set its indent to 0. 
+ *
+ * 2. Call the make output interface. 
  */
 void AbstractParser::processOutput()
 {
@@ -246,8 +368,22 @@ void AbstractParser::processOutput()
 
 
 /*!
+ * Write the processed output to the given file if, after joining the output 
+ * lines, it is different from the original. If the output is the same as the 
+ * original then this does nothing. 
  *
- * @param file  
+ * @param file The file that is truncated and written to if the output is 
+ *             different from the original. 
+ *
+ *
+ * Steps of Operation: 
+ *
+ * 1. Join the output lines into one giant string. If the output is different 
+ *    from the root parser object's original file content then go to the next 
+ *    step, else do nothing and exist the function. 
+ *
+ * 2. Truncate the given file to 0 and write the root parser object's output to 
+ *    it. If any error occurred for either operation then throw an exception. 
  */
 void AbstractParser::write(QFile* file)
 {
--- src/abstractparser.h
+++ src/abstractparser.h
@@ -10,6 +10,14 @@ class QFile;
 
 
 /*!
+ * This represents a parser object which takes in a single source code file to 
+ * parse and outputs the results, overwriting the file if the results are 
+ * different from the original contents. It can contain child parser objects 
+ * which can be used for parsing segments of the source file allowing for 
+ * specializing of parsing within segments of the file. Children parser objects 
+ * will write lines to its root parser object. This allows for parsing the same 
+ * file but using different parser object types when different parts of the file 
+ * are reached. 
  */
 class AbstractParser : public QObject
 {
@@ -20,15 +28,26 @@ public:
    virtual ~AbstractParser();
    void execute(QFile* file);
 protected:
+   virtual void initialize();
    /*!
+    * This interface is called for each line of the input source file, reading in 
+    * all lines of the file until the end is reached or this returns false. If 
+    * false is returned and this is a child parser then this interface keeps 
+    * getting called on this object's parent parser under the same conditions. A 
+    * different function is used to signal this parser should start feeding input 
+    * lines to a child parser this parser contains. 
     *
-    * @param line  
+    * @param line The line from the source file that is processed. 
+    *
+    * @return True signals this interface should continue being called on lines 
+    *         else false signals this parser is finished processing input. 
     */
    virtual bool readLine(const QString& line) = 0;
    /*!
+    * This interface creates the output lines for the source file, using the add 
+    * and indent functions to do so. 
     */
    virtual void makeOutput() = 0;
-   virtual void initialize();
    void stepIntoChild(AbstractParser* child);
    int indent();
    void setIndent(int indent);
@@ -41,27 +60,48 @@ private:
    void processOutput();
    void write(QFile* file);
    /*!
+    * This stores the original content of the file that is used to compare the 
+    * output once parsing is complete. 
     */
    QString _original;
    /*!
+    * This stores the input lines of the parsed source file. All parsing object's 
+    * with the same root share this as a single resource where the root parser is 
+    * responsible for allocating and destroying it. 
     */
    QStringList* _input {nullptr};
    /*!
+    * This stores the index pointing to the next input line to be processed. All 
+    * parsing object's with the same root share this as a single resource where the 
+    * root parser is responsible for allocating and destroying it. 
     */
    int* _index {nullptr};
    /*!
+    * This stores the indent that is used for prepending spaces to all output lines 
+    * when they are added. All parsing object's with the same root share this as a 
+    * single resource where the root parser is responsible for allocating and 
+    * destroying it. 
     */
    int* _indent {nullptr};
    /*!
+    * This stores the output lines of the parsed file. All parsing object's with 
+    * the same root share this as a single resource where the root parser is 
+    * responsible for allocating and destroying it. 
     */
    QStringList* _output {nullptr};
    /*!
+    * This is a pointer to this parser object's root parser object. If this parser 
+    * is the root then this is null. 
     */
    AbstractParser* _root {nullptr};
    /*!
+    * This stores a list of pointers to all of this parser object's children. 
     */
    QList<AbstractParser*> _children;
    /*!
+    * This is a special pointer signal variable set by the step into child 
+    * function. It is used to signal the process input function that it should step 
+    * into the pointed to child parser object's process input function. 
     */
    AbstractParser* _child {nullptr};
 };
