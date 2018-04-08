ca305751ac075e6c4c5cfc023e5a13f6f155d08a
blaidddwr <blaidddwr@protonmail.com>
2018-04-08 15:13:17 -0700

    Modified application.
    Updated revision number for 0.4.4 release.

--- src/application.h
+++ src/application.h
@@ -27,7 +27,7 @@ public:
    /*!
     * Revision number of this program. 
     */
-   constexpr static int _revision {3};
+   constexpr static int _revision {4};
    /*!
     * The company key used to access this program's qt settings. 
     */
