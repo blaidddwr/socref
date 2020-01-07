30917d7ad3ff7838efc2a07e666eaf08765a9037
blaidddwr <blaidddwr@protonmail.com>
2020-01-07 15:52:32 -0800

    * Modified setup script.
    setup:
    Imported settings from core application and used its version string for setup
    tools version.

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ install.
 """
 from setuptools import setup
 from setuptools import find_packages
+from socref import settings
 
 setup(
     name="socref"
-    ,version="0.0.9999"
+    ,version=settings.VERSION
     ,packages=find_packages()
     ,install_requires=["hunspell"]
     ,entry_points={"gui_scripts": ["socref = socref.__main__:main"]}
