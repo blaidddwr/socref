5e4ff298d995568a042d4fc73194e97d06998e0d
blaidddwr <blaidddwr@protonmail.com>
2025-02-19 09:01:56 -0800

    build+: Added missing source file.
    build+(Cpp): Added missing source file for C++ namespace. This should
    have been added in commit 5d7f369c0561ebef31328153e0f716a8e84b0e45.
    
    GitHub Issue #10
    Add C++/Qt Language Parsers

---
+++ src/cpplib/Cpp.cpp
@@ -0,0 +1,18 @@
+#include "Cpp.h"
+#include <QStringList>
+namespace Cpp {
+
+
+const QString& codeKey(
+    int index
+)
+{
+    static const QStringList keys
+    {
+        "preProcessHead"
+    };
+    Q_ASSERT(index >= 0);
+    Q_ASSERT(index < keys.size());
+    return keys.at(index);
+}
+}
